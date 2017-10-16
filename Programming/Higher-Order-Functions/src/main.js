c440bf709e5f4e6449fa8e3a2cf8ca7c95afe517
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-10-16 15:35:44 -0600

    ★  passing tests for any()

--- Programming/Higher-Order-Functions/src/main.js
+++ Programming/Higher-Order-Functions/src/main.js
@@ -6,17 +6,17 @@
 // e.g. any([-1,-2,0], function (num) { return num > 0 })
 //      >> false
 function any (arr, callback) {
-  // callback(5)
-  for(var i = 0; i < arr.length; i++){
-    //console.log(arr[i])
-    // return callback(arr[i])
-    callback(arr[i])
+  if(arr.length === 0){
+    return false
+  } else {
+    for(var i = 0; i < arr.length; i++){
+      return callback(arr[i])
+    }
   }
 }
 
 var myArray = [-1, 2, 0]
 var myCallBackFunction = function(num){
-  console.log(num > 0)
    return num > 0
  }
 
@@ -29,8 +29,19 @@ any(myArray, myCallBackFunction)
 // NOTE: You only need to account for '+', '-', '*', and '/'
 //       If it is not one of those, return false
 function higherOrderMath (num1) {
-
-  return false
+  var operator
+  var number
+  if num1 === null{
+    return 
+  }
+  if else (num1 !== isNaN()){
+    var number =  num1
+  } else {
+    var number = parseInt(num1)
+  }
+  return number += higherOrderMath(num1)
 }
+
+higherOrderMath(10)('+')('11')
 
 module.exports = { any, higherOrderMath }
